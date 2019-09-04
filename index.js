9808fc3b1e24d13b3791b14442e983c821c35eb4
jenmyers <jen.myers@flatironschool.com>
2019-09-04 15:22:24 -0500

    Add solution

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
 }
