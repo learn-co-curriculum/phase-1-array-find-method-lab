ca6db7e6e17c0dc82a44a06accc643344ddeb09a
Hpollard003 <hpollard003@gmail.com>
2021-03-25 18:31:30 -0400

    commit tree f88a0ccbb4bd2807d1bab3b8bec2b65a35d34efd

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function superbowlWin(record) {
+  const result = record.find(isWin)
+  if(Boolean(result) === true) {
+    return result.year
+  }
+}
 
+function isWin(gameObj) {
+  return gameObj.result === "W"
+}
