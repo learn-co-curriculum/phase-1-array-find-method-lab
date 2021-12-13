187f92b657c2e964d02e338a279d7bb2909ab5a2
Gracie <m.graciemcguire@gmail.com>
2021-12-13 13:41:25 -0500

    removes reference to racist team name

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,9 +4,9 @@ describe('superbowlWin(record)', function () {
     { year: "2018", result: "N/A"},
     { year: "2017", result: "N/A"},
     { year: "2016", result: "N/A"},
-    { year: "2015", result: "N/A"},
+    { year: "2015", result: "W"},
     { year: "2014", result: "N/A"},
-    { year: "2013", result: "N/A"},
+    { year: "2013", result: "L"},
     { year: "2012", result: "N/A"},
     { year: "2011", result: "N/A"},
     { year: "2010", result: "N/A"},
@@ -21,8 +21,8 @@ describe('superbowlWin(record)', function () {
     { year: "2001", result: "N/A"},
     { year: "2000", result: "N/A"},
     { year: "1999", result: "N/A"},
-    { year: "1998", result: "N/A"},
-    { year: "1997", result: "N/A"},
+    { year: "1998", result: "W"},
+    { year: "1997", result: "W"},
     { year: "1996", result: "N/A"},
     { year: "1995", result: "N/A"},
     { year: "1994", result: "N/A"},
@@ -30,10 +30,10 @@ describe('superbowlWin(record)', function () {
     { year: "1992", result: "N/A"},
     { year: "1991", result: "N/A"},
     { year: "1990", result: "N/A"},
-    { year: "1989", result: "N/A"},
+    { year: "1989", result: "L"},
     { year: "1988", result: "N/A"},
-    { year: "1987", result: "N/A"},
-    { year: "1986", result: "N/A"},
+    { year: "1987", result: "L"},
+    { year: "1986", result: "L"},
     { year: "1985", result: "N/A"},
     { year: "1984", result: "N/A"},
     { year: "1983", result: "N/A"},
@@ -43,17 +43,17 @@ describe('superbowlWin(record)', function () {
     { year: "1979", result: "N/A"},
     { year: "1978", result: "N/A"},
     { year: "1977", result: "N/A"},
-    { year: "1976", result: "N/A"},
+    { year: "1976", result: "L"},
     { year: "1975", result: "N/A"},
     { year: "1974", result: "N/A"},
     { year: "1973", result: "N/A"},
     { year: "1972", result: "N/A"},
     { year: "1971", result: "N/A"},
     { year: "1970", result: "N/A"},
-    { year: "1969", result: "W"},
+    { year: "1969", result: "N/A"},
     { year: "1968", result: "N/A"},
     { year: "1967", result: "N/A"},
-    { year: "1966", result: "L"},
+    { year: "1966", result: "N/A"},
     { year: "1965", result: "N/A"},
     { year: "1964", result: "N/A"},
     { year: "1963", result: "N/A"},
@@ -62,8 +62,8 @@ describe('superbowlWin(record)', function () {
     { year: "1960", result: "N/A"}
   ]
 
-  it('returns a year the KC Chiefs won the superbowl', function () {
-    expect(superbowlWin(record)).to.equal('1969');
+  it('returns a year the Denver Broncos won the superbowl', function () {
+    expect(superbowlWin(record)).to.equal('1976');
   });
 
   it('returns undefined if the record has no win objects', function() {
