d81cc1415f5ae5d71b076e2fc855959cb32fdd07
Eric Einerson <eaeiner.gmail.co>
2022-01-13 10:33:27 -0700

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const dogs = ["Golden Retriever", "Pit Bull", "Chow"]
+
+function sayDogIsCute(dog) {
+    console.log(`The ${dog} is so cute`);
+}
+
+dogs.forEach(sayDogIsCute);
+
+function loopThroughAllDogs(allDogs) {
+    allDogs.forEach(sayDogIsCute);
+}
+
+loopThroughAllDogs(dogs)
+loopThroughAllDogs()
